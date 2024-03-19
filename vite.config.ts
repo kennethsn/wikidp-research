7d2aaf8d04aa30ffe211c26f20ab66f95cf9ae0b
Kenneth Seals-Nutt <kenneth@seals-nutt.com>
2024-03-19 08:12:16 -0400

    CONFIG: Ensure Env vars work in production build

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 
+// @ts-ignore
+const inProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   plugins: [
-    EnvironmentPlugin([
-      'OPENAI_API_KEY',
-      'STORIES_SERVICES_API_BASE_URL',
-      'STORIES_SERVICES_DISABLED',
-    ]),
+    EnvironmentPlugin({
+      OPENAI_API_KEY: '',
+      STORIES_SERVICES_API_BASE_URL: '',
+      STORIES_SERVICES_DISABLED: '',
+    }, { loadEnvFiles: !inProd }),
     react({ jsxImportSource: '@emotion/react' }),
   ],
 });
